dc5860bacb22de2af171f5595643157c0d49dd9c
Ravindra Ravu <rnravu@protonmail.com>
2026-03-08 08:28:07 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a message to send to ConsoleApp2:");
+            string userInput = Console.ReadLine();
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "ConsoleApp2.exe";
+            startInfo.Arguments = userInput;
+            startInfo.UseShellExecute = false;
+
+            Process process = Process.Start(startInfo);
+            process.WaitForExit();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string message = string.Join(" ", args);
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
